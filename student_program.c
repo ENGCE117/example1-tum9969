8f652c6d5d6cf8746845b860d36510b660e3ce40
tum9969 <135408624+tum9969@users.noreply.github.com>
2025-01-17 23:15:58 +0700

    Update student_program.c

--- student_program.c
+++ student_program.c
@@ -6,7 +6,7 @@ int main() {
     if( a % 2 == 0 ) 
         for( int i = 1 ; i <= a ; i++ ) printf( "%d ", i * 2 )  ;
     else 
-        for( int i = a ; i >= 1 ; i-- ) printf( "%d ", i )  ;
+        for( int i = 9 ; i >= 1 ; i -= 2 ) printf( "%d ", i )  ;
     
     return 0 ;
 }//end function
