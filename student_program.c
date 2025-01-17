12a16bc48b5aeb2db1a7aa5d929b7995c9c9b3ae
tum9969 <135408624+tum9969@users.noreply.github.com>
2025-01-17 14:19:22 +0700

    Update student_program.c

--- student_program.c
+++ student_program.c
@@ -4,7 +4,7 @@ int main() {
     int a = 0 ;
     scanf( "%d", &a ) ;
     for( int i = 0 ; i < a ; i++ ) {
-        if( i % 2 == 0 ) printf( "%d ", i )  ;
+        printf( "%d ", i )  ;
     }
     return 0 ;
 }//end function
