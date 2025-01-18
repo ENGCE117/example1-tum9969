980f36f15170cb1e7dc057d7e3f366f1d509d64c
tum9969 <135408624+tum9969@users.noreply.github.com>
2025-01-18 12:59:54 +0700

    Update student_program.c

--- student_program.c
+++ student_program.c
@@ -5,7 +5,7 @@ int main() {
     printf( "Enter Number : " ) ;
     scanf( "%d %d", &a, &b ) ;
     if( a == 0 && b == 0 ) answer = 0 ;
-    else if( a == 1 && b == 1 ) answer = 1 ;
+    else if( a == 1 && b == 1 ) answer = 0 ;
     else answer = a + b ;
     printf( "Answer = %d", answer ) ;
     return 0 ;
